cd2acd1ec7afea4b28700ecac0b0faa3804d10d5
FOAMLabs <web3welfarequeen@proton.me>
2023-10-19 00:49:15 -0700

    if you don't succeed... then try try again

--- components/Home/hooks/useSaveUserData.ts
+++ components/Home/hooks/useSaveUserData.ts
@@ -1,25 +1,32 @@
-import { sql } from "@vercel/postgres";
-import type { NextApiRequest, NextApiResponse } from 'next';
+// hooks/useSaveUserData.ts
 
-export type UserData = {
+type UserData = {
     userID: number;
     address: string;
     ipAddress: string;
 };
 
+
 export const useSaveUserData = () => {
     const saveUserDataToDB = async (userData: UserData): Promise<void> => {
-        try {
-            const { rows } = await sql`
-                INSERT INTO userData (userID, address, ipAddress)
-                VALUES (${userData.userID}, ${userData.address}, ${userData.ipAddress})
-                RETURNING *;
-            `;
-            console.log('User data saved:', rows[0]);
-        } catch (error) {
-            console.error('Error saving user data:', error);
+      try {
+        const response = await fetch('/api/saveUserData', {
+          method: 'POST',
+          body: JSON.stringify(userData),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (!response.ok) {
+          console.error('Failed to save user data:', response.statusText);
+          return;
         }
+        const responseData = await response.json();
+        console.log('User data saved:', responseData);
+      } catch (error) {
+        console.error('Error saving user data:', error);
+      }
     };
-
+    
     return { saveUserDataToDB };
-};
+  };
