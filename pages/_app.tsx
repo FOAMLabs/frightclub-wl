fdc7d0965b0d10a5e0355391ed253a7e375afe3d
FOAMLabs <web3welfarequeen@proton.me>
2023-10-19 15:28:19 -0700

    cleaned up repo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,15 +11,7 @@ import {
 import type { AppProps } from "next/app";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 
-import {
-  arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  base,
-  zora,
-} from "wagmi/chains";
+import { goerli, mainnet } from "wagmi/chains";
 
 import { publicProvider } from "wagmi/providers/public";
 import merge from "lodash.merge";
@@ -36,11 +28,6 @@ const myTheme = merge(darkTheme(), {
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
     mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    zora,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
   ],
   [publicProvider()]
